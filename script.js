6f124f5368e9165f39b1447f79ad8390cab76dfe
Saiissh <118446344+Saiissh@users.noreply.github.com>
2023-06-13 16:16:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', handleDOMLoad);
+
+function handleDOMLoad() {
+  var paragraph = document.createElement('p');
+  var text = document.createTextNode('DOM load success');
+  paragraph.appendChild(text);
+  document.body.appendChild(paragraph);
+}
